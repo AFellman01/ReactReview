cb69c51f6df118b8faa14e0e29bd6ffe364351a1
Alexander Fellman <alexanderfellman@The-Wonderful-Wombat-of-Oz.local>
2018-05-28 11:50:25 -0400

    First commit

---
+++ src/components/HelloReact.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const HelloReact = () => <p>Hello world, this is me life should be
+Fun for everyone
+Life is easy if you wear a smile
+Just be your self don't ever change your style You are you, I am me, we'll be free
+
+Hello World</p>;
+
+export default HelloReact;
